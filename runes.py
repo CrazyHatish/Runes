584b41ca0ca84c060a537bc89e2be4e1ceb5b09c
CrazyHatish <phg1608@gmail.com>
2017-11-18 18:56:27 +0100

    Make script not mess with client scale

--- runes.py
+++ runes.py
@@ -1,9 +1,9 @@
 import pyautogui as pygui
+import json
 from bs4 import BeautifulSoup
 from re import compile
 from tkinter import Tk, StringVar
 from tkinter.ttk import Combobox, Button
-import json
 from time import sleep, time
 from requests import get
 from ctypes import windll, wintypes, byref
@@ -18,12 +18,20 @@ def get_taskbar_size():
         GetSystemMetrics = windll.user32.GetSystemMetrics
         return GetSystemMetrics(SM_CYSCREEN) - work_area.bottom
 
-screen_size = pygui.size()
-client = (screen_size[0]/2 - 1024/2, (screen_size[1]-get_taskbar_size())/2 - 576/2)
+client = 0
 
 
 def main():
     champs = []
+    scale = 0.8
+
+    try:
+        with open('/Riot Games/League of Legends/Config/LCULocalPreferences.yaml') as file:
+            for line in file:
+                if "ZoomScale" in line:
+                    scale = float(line[-9:])
+    except FileNotFoundError:
+        pass
 
     try:
         with open('champs.json') as file:
@@ -40,6 +48,10 @@ def main():
         get_runes('runes.json')
         with open('runes.json') as file:
             rn = json.load(file)
+
+    screen_size = pygui.size()
+    global client
+    client = (screen_size[0]/2 - (1024 * scale/0.8)/2, (screen_size[1]-get_taskbar_size())/2 - (576 * scale/0.8)/2)
 
 
     root = Tk()
@@ -63,7 +75,11 @@ def main():
     tree_choose = Combobox(root, textvariable=tree, values=['Mais Popular', 'Melhor Winrate'], width=15)
     tree_choose.place(x = 200, y=20)
 
-    button = Button(root, text="Start", command= lambda: make_page(get_tree(tkvar.get(), pos.get(), tree.get(), rn), lang.get()))
+    button = Button(root, text="Start", command= lambda:
+                                                    make_page(
+                                                        get_tree(tkvar.get(), pos.get(), tree.get(), rn),
+                                                        lang.get(), scale)
+                                                        )
     button.place(x=250, y=70)
 
     root.mainloop()
@@ -91,30 +107,37 @@ def get_tree(champion, pos, tree, rn):
 
     return runes
 
-def make_page(runes, lang):
-    coords = {
+def make_page(runes, lang, scale):
+    l_coords = {
         "en": [(485, 98), (459, 98)],
         "pt": [(501, 98), (471, 98)],
         }
 
+    coords = [
+        (326, 549), l_coords[lang][0],
+        (484, 306), l_coords[lang][1],
+        'sleep',
+        (170*runes[0], 183), (203 + 54*(runes[1]-1), 262),
+        (203 + 54*(runes[2]-4), 340), (203 + 54*(runes[3]-7), 411),
+        (203 + 54*(runes[4]-10), 482), (457 + 39*(runes[5]-1), 168),
+        (463 + 54*((runes[6]-4)%3), 237 + 63*((runes[6]-4)//3)),
+        (463 + 54*((runes[7]-4)%3), 237 + 63*((runes[7]-4)//3)),
+    ]
+
     sleep(1)
     pygui.hotkey('ctrl', '=')
     [pygui.hotkey('ctrl', '-') for _ in range(3)]
+    if scale > 0.8:
+        pygui.hotkey('ctrl', '=')
+        if scale > 1:
+            pygui.hotkey('ctrl', '=')
     sleep(1)
 
-    click_client(326, 549)
-    click_client(coords[lang][0][0], coords[lang][0][1])
-    click_client(484, 306)
-    click_client(coords[lang][1][0], coords[lang][1][1])
-    sleep(1)
-    click_client(170*runes[0], 183)
-    click_client(203 + 54*(runes[1]-1), 262)
-    click_client(203 + 54*(runes[2]-4), 340)
-    click_client(203 + 54*(runes[3]-7), 411)
-    click_client(203 + 54*(runes[4]-10), 482)
-    click_client(457 + 39*(runes[5]-1), 168)
-    click_client(463 + 54*((runes[6]-4)%3), 237 + 63*((runes[6]-4)//3))
-    click_client(463 + 54*((runes[7]-4)%3), 237 + 63*((runes[7]-4)//3))
+    for coord in coords:
+        if coord == 'sleep':
+            sleep(1)
+        else:
+            click_client(coord[0] * scale/0.8, coord[1] * scale/0.8)
 
 
 def click_client(x, y):
